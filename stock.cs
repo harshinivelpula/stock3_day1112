f76cac6d730b23b8afa3e64e3645671d6a8ac900
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-10-23 20:27:41 +0530

    cal each value of stock

---
+++ stock.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day1112
+{
+    internal abstract class stock
+	{
+		public abstract void names();
+        {
+       "goodday": 
+		{
+			"stock_name": "goodday_company",
+			"number_of_shares": 120,
+			"share_price": 5
+		}
+	
+	"marie": 
+		{
+			"stock_name": "britania_company",
+			"number_of_shares": 150,
+			"share_price": 10
+		}
+	
+  "oreo": 
+		{
+			"stock_name": "oreo_company",
+			"number_of_shares": 110,
+			"share_price": 20
+		}
+	
+	"tiger": 
+		{
+			"stock_name": "tiger_company",
+			"number_of_shares": 150,
+	       "share_price": 10
+		}
+	
+    }
+
+}
+
+	
